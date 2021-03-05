996897cecad75438cc3217629fe6cbd6af7f7c65
Diego Raul <devup2332@gmail.com>
2021-03-05 16:53:31 -0500

    Added products section with responsive styles for all screens

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -6,10 +6,11 @@ import { HomeComponent } from './home.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AboutComponent } from './components/about/about.component';
 import { GalleryImagesComponent } from './components/gallery-images/gallery-images.component';
+import { ProductsComponent } from './components/products/products.component';
 
 
 @NgModule({
-  declarations: [HomeComponent, BannerComponent, AboutComponent, GalleryImagesComponent],
+  declarations: [HomeComponent, BannerComponent, AboutComponent, GalleryImagesComponent, ProductsComponent],
   imports: [
     CommonModule,
     HomeRoutingModule
